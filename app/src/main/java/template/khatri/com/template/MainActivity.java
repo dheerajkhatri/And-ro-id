80c3e4231a6ff5c538ee2fe9f44ea09ad00322df
dheerajkhatri <er.dheerajkhatri@gmail.com>
2018-04-08 00:39:42 +0530

    added multiple activity support and button send support

--- app/src/main/java/template/khatri/com/template/MainActivity.java
+++ app/src/main/java/template/khatri/com/template/MainActivity.java
@@ -1,5 +1,6 @@
 package template.khatri.com.template;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "template.khatri.com.template.Message";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +29,14 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText3);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 
     @Override
